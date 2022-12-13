afa031841c7ca3f7abf16b16ae7d158e8d81478d
Hiren <rebeldevmurari@gmail.com>
2022-12-13 16:58:02 +0000

    Implemented lambda function

--- src/com/rebel/demo/Main.java
+++ src/com/rebel/demo/Main.java
@@ -6,6 +6,33 @@ public class Main
 	public static void main(String[] args) {
 	
 		System.out.println("this is lambda lessons");
+		System.out.println();
+		
+		AddTwoi a = () -> System.out.println("this is first time using Lamda");
+			
+		a.hry();
+		System.out.println();
+		
+		AddTwoi at = new AddTwoi() {
+			
+			@Override
+			public void hry() {
+				System.out.println("this is annoymous fetching interface");
+				
+			}
+		};
+		
+		at.hry();
+		System.out.println();
+		
+		Impl i = new Impl();
+		i.hry();
+		
+		System.out.println();
+		
+		//again fetchgng through the lambda
+		
+		 
 		
 
 	}
